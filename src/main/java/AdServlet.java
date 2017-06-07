70db6b82058e3ba724a20aa1144b6976cd3ae409
frenchfryes <derekwfrye@gmail.com>
2017-06-07 16:05:37 -0500

    created servlet

---
+++ src/main/java/AdServlet.java
@@ -0,0 +1,17 @@
+/**
+ * Created by frenchfryes on 6/7/17.
+ */
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "AdServlet", urlPatterns = "/ads")
+public class AdServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("ads", DaoFactory.getAdsDao().all());
+        request.getRequestDispatcher("/ads/index.jsp").forward(request, response);
+    }
+}
